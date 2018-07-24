58141a13604873564fcac19b8a8c05bd32b5d9d8
york17 <317860079@qq.com>
2018-07-25 00:26:57 +0800

    1.添加天气页面与数据；

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.coolweather.android;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -89,6 +90,13 @@ public class ChooseAreaFragment extends Fragment {
                     //市
                     selectedCity = cityList.get(position);
                     queryCountries();
+                } else if ( currentLevel == LEVEL_COUNTRY ) {
+                    //县
+                    String weatherId = countryList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
